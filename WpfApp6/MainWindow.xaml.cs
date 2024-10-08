0f0cd1ae79590d969187549e809c8beb65459f6d
Alex Pushkin <ifc@suz-ppk.ru>
2024-10-08 14:37:42 +1000

    update

--- WpfApp6/MainWindow.xaml.cs
+++ WpfApp6/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -31,27 +32,29 @@ namespace WpfApp6
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Result"));
             }
         }
+
+        HttpClient httpClient = new HttpClient();
+
         public MainWindow()
         {
             InitializeComponent();
+            httpClient.BaseAddress = new Uri("http://localhost:5094/api/");
             DataContext = this;
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        private async void Test(object sender, RoutedEventArgs e)
+        private async void Test_Get(object sender, RoutedEventArgs e)
         {
             //http://localhost:5094/api/Math/КрутоеИмя?x=11&y=11
-
-            HttpClient httpClient = new HttpClient();
-            httpClient.BaseAddress = new Uri("http://localhost:5094/api/");
+            
             /*var responce = await httpClient.GetAsync
                 ($"Math/КрутоеИмя?x={X}&y={Y}");
             Result = await responce.Content.ReadAsStringAsync();*/
 
             var responce = await httpClient.GetAsync
                 ($"Math/НереальноКрутоеИмя?x={X}&y={Y}");
-            if (responce.StatusCode == System.Net.HttpStatusCode.NotFound)
+            if (responce.StatusCode != System.Net.HttpStatusCode.OK)
             {
                 Result = await responce.Content.ReadAsStringAsync();
                 return;
@@ -70,13 +73,36 @@ namespace WpfApp6
 
 
         }
+
+        private async void Test_Post(object sender, RoutedEventArgs e)
+        {
+            MultiplyResult multiplyResult = new MultiplyResult
+            {
+                X = this.X, Y = this.Y
+            };
+            string arg = JsonSerializer.Serialize(multiplyResult);
+            var responce = await httpClient.PostAsync($"Math/НереальноКрутоеИмяСоСложнымАргументом",
+                new StringContent(arg, Encoding.UTF8, "application/json"));
+
+            if(responce.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                Result = await responce.Content.ReadAsStringAsync();
+                return;
+            }
+            else
+            {
+                var answer = await responce.Content.ReadFromJsonAsync<MultiplyResult>();
+                Result = answer.ToString();
+            }
+
+        }
     }
 
     public class MultiplyResult
     {
         public int X { get; set; }
-        public string Y { get; set; }
-        public string Result { get; set; }
+        public string? Y { get; set; }
+        public string? Result { get; set; }
 
         public override string ToString()
         {
